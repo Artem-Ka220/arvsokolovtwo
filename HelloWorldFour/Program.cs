7260e9dd6fbf5e0cd6b33a41cb5a68564eaaf579
arvsokolov <marktwe@mail.ru>
2023-05-14 17:44:12 +0300

    Added new file HelloWorldFour

---
+++ HelloWorldFour/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введит первое число: ");
+int firstNomber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вы ввели: " + (firstNomber));
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вы ввели: " + secondNumber);
+Console.WriteLine("А сумма этих чисел: " + (firstNomber + secondNumber) + ".");
+
